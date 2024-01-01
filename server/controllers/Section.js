aca7e0c2a21d530219225409aefff1ec59369119
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-01 19:36:51 +0530

    section controller created sucessfully

---
+++ server/controllers/Section.js
@@ -0,0 +1,86 @@
+const Section = require("../models/Section")
+const Course= require("../models/Course")
+
+exports.createSection=async(req,res)=>{
+
+   try {
+     // fetch the data
+     const {sectionName,courseId}=req.body;
+
+     // validate
+     if (!sectionName||!courseId) {
+         return res.status(401).json({
+             message:"please Enter all the fields properly"
+         })
+     }
+     // create a entry in DB
+     const sectionData= Section.create({
+         sectionName:sectionName
+     })
+     // update course with section object Id
+     const updatedCourse = await Course.findByIdAndUpdate(courseId,{
+         $push:{courseContent:sectionData._id}
+     },{new:true})
+     // return Response 
+ 
+     return res.status(200).json({
+         message:"Section created successfully ",
+         success:true
+     })
+   } catch (error) {
+    return res.status(500).json({
+        message:"Section canot be created",
+        success:false
+    })
+   }
+}
+
+// update section
+exports.updateSection=async(req,res)=>{
+
+ try {
+       // fetch data
+       const {sectionName,sectionId}=req.body;
+
+       // validate
+       if (!sectionName||!sectionId) {
+           return res.status(401).json({
+               message:"please Enter all the fields properly"
+           })
+       }
+           // update section
+           const updatedSection = await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+           // return response
+
+           return res.status(200).json({
+            message:"Section updated successfully ",
+            success:true
+        })
+ } catch (error) {
+    return res.status(500).json({
+        message:"Section canot be updated",
+        success:false,
+        error:error.message
+    })
+ }
+
+}
+// delete section
+
+exports.deleteSection=async(req,res)=>{
+
+    try {
+        const {sectionId}=req.params;
+        await Section.findByIdAndDelete(sectionId);
+        return res.status(200).json({
+            message:"Section Deleted successfully ",
+            success:true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Section canot be deleted",
+            success:false,
+            error:error.message
+        })  
+    }
+}
