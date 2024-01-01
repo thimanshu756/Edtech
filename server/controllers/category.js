28863b5e7d37d2821c335816c6513cab02188e1d
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-01 19:16:10 +0530

    replaced tag with category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,8 +1,8 @@
 
-const Tag = require("../models/Tags")
+const Category = require("../models/Category")
 
 // tag handler
-exports.createTAg=async(req,res)=>{
+exports.createCategory=async(req,res)=>{
 
     try {
         // fetch data from req.body
@@ -14,36 +14,37 @@ exports.createTAg=async(req,res)=>{
                 message:"Please enter all the fields properly and try again"
             })
             // create entry in db
-            const tagData= await Tag.create({
+            const categoryData= await Category.create({
                 name:name,
                 description:description
-            });
-            console.log(tagData);
+            })
+            console.log(categoryData);
             return res.status(200).json({
-                mesage:"tag created successfully",
+                mesage:"Category created successfully",
                 success:true
             })
         }
     } catch (error) {
         return res.status(500).json({
-            message:"got an error in creating tag",
-            success:false
+            message:"got an error in creating a Category",
+            success:false,
+        categoryData
         })
     }
 }
 
 // get all the tags
-exports.showallTags=async(req,res)=>{
+exports.showallCategory=async(req,res)=>{
     try {
-        const allTags= await Tag.find({},{name:true,description:true});
+        const allCategories= await Category.find({},{name:true,description:true});
         return res.status(200).json({
-            message:"Successfully fetched all tags",
+            message:"Successfully fetched all categories",
             success:true,
-            allTags
+            allCategories
         })
     } catch (error) {
         return res.status(500).json({
-            message:"Getting error in fetching the tags",
+            message:"Getting error in fetching the categories",
             success:false
         })
     }
---
+++ server/controllers/course.js
@@ -0,0 +1,75 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Category");
+const User = require("../models/Users");
+const { uploadToCloudinary } = require("../utils/imageUploader");
+
+
+
+const createCourse=async(req,res)=>{
+
+    try {
+            // fetch data
+        const {courseName,courseDescription,whatYouWillLearn, price,tag}=req.body;
+
+        // get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+        // validation
+        if (!courseName||!courseDescription||!whatYouWillLearn||!price||!tag) {
+            return res.status(401).json({
+                message:"Please fill all the data correctly",
+                success:false,
+            })
+        }
+        // check for instructor
+        const userId = req.user.id;
+        const InstructorDetails = await User.findById(userId);
+        if (!InstructorDetails) {
+           return res.status(401).json({
+                message:"Instructor details not found",
+                success:false
+            })
+        }
+        const Tagdetails = await Tag.findById(tag);
+        if (!Tagdetails) {
+            return res.status(401).json({
+                message:"Tag details not found",
+                success:false
+            })
+        }
+        // upload img to  cloudinary
+        const thumbnailImage = await uploadToCloudinary(thumbnail,process.env.FolderName);
+
+        // create an entry for new course
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor:InstructorDetails._id,
+            whatYouWillLearn:whatYouWillLearn,
+            price,
+            tag:Tagdetails._id,
+            thumbnail:thumbnailImage.secure_url,
+        })
+        console.log(newCourse);
+        
+        // ad a new course to user schema of instructor
+        const newInstructorCourse= await User.findByIdAndUpdate({id:InstructorDetails._id},{
+            $push:{
+                courses:newCourse._id
+            },
+           
+        },{new:true}
+        )
+        console.log(newInstructorCourse);
+        // update the Tag Schema :Todo
+        return res.status(200).json({
+            message:"course created ",
+            success:true
+        })
+    } catch (error) {
+        return res.status(401).json({
+            message:"Error in creating Course",
+            success:false
+        })
+    }
+
+}
