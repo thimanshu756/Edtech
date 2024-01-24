602b83c042ff42087a64171e094e517fbbe06969
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-24 19:49:28 +0530

    Text highlight created

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [Instructor , setInstructor]= useState(false);
+    
   return (
-    <div>
+    <div className=' w-11/12 bg-richblack-900 max-w-maxContent flex '>
+        {/* form */}
+        <div className='flex w-[40%] p-[32px] flex-col gap-[36px]'>
+        <div>
+            <p>Join the millions learning to code with StudyNotion for free</p>
+            <p></p>
+        </div>
+        </div>
+        {/* image */}
+        <div>
 
+        </div>
     </div>
   )
 }
