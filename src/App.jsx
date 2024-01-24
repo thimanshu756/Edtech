c0d247e3109d5ead944820612594aef9f8e3d4ee
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-24 19:28:47 +0530

    signup component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Routes , Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        {/* <Route path="/login" element={<Login/>}/> */}
       </Routes>
     </div>
   );
