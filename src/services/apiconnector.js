8cc413c6fb50f8c54834feddda5ab8b1aca3f568
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-26 18:54:58 +0530

    apiconnector created

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,2 +1,13 @@
 import axios from "axios";
 
+const axiosInstance = axios.create({});
+
+export const apiconnector = (method ,url,bodyData,headers, params)=>{
+    return axiosInstance({
+        method:`${method}`,
+        url:`${url}`,
+        data:bodyData ? bodyData :null,
+        headers:headers ? headers:null,
+        params:params ?params:null
+    });
+}
