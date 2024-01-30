9e97e4c22fb5226dd7cbc43a03ee12f3300bd22d
thimanshu756 <himanshu1662.be22@gmail.com>
2024-01-30 15:22:11 +0530

    Open Route updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,40 @@ import Navbar from "./Components/Common/Navbar";
 import VerifyEmail from "./Pages/VerifyEmail";
 import ForgotPassword from "./Pages/ForgotPassword";
 import UpdatePassword from "./Pages/UpdatePassword";
-
+import OpenRoute from "./Components/Core/AuthAcesses/OpenRoute";
+import PrivateRoute from "./Components/Core/AuthAcesses/PrivateRoute";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/login" element={<Login/>}/>
+
+        <Route path="/signup" element={
+        <OpenRoute>
+            <SignUp/>
+        </OpenRoute>
+        }/>
+
+   
+        <Route path="/login" element={
+          <OpenRoute>
+              <Login/>
+          </OpenRoute>
+    
+        }/>
         <Route path="/verify-email" element={<VerifyEmail/>}/>
-        <Route path="forgot-password" element={<ForgotPassword/>}/>
-        <Route path="update-password/:id" element={<UpdatePassword/>}/>
+
+        <Route path="forgot-password" element={
+        <OpenRoute>
+        <ForgotPassword/>     
+        </OpenRoute>
+     }/>
+        <Route path="update-password/:id" element={
+        <OpenRoute>
+        <UpdatePassword/>  
+        </OpenRoute>
+        }/>
       </Routes>
       <Footer/>
     </div>
--- src/Components/Core/AuthAcesses/OpenRoute.jsx
+++ src/Components/Core/AuthAcesses/OpenRoute.jsx
@@ -1,11 +1,11 @@
 import React, { Children } from 'react'
 import { useSelector} from 'react-redux'
 import { Navigate } from 'react-router-dom'
-const OpenRoute = () => {
+const OpenRoute = ({children}) => {
     const {token}=useSelector((state)=>state.auth)
     
     if (token==null) {
-        return Children
+        return children
     }else{
         return <Navigate to="/dashboard/my-profile"></Navigate>
     }
