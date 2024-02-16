057593669b179246a79454efd1ffdcaa4e12fd29
thimanshu756 <himanshu1662.be22@gmail.com>
2024-02-16 17:32:47 +0530

    CourseDetailsPage created

---
+++ src/Pages/CourseDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const CourseDetails = () => {
+    const courseId = useParams();
+    
+  return (
+    <div>CourseDetails</div>
+  )
+}
+
+export default CourseDetails
