42c7eb16c75d7a1fdf5711c45460828f494e9a3b
thimanshu756 <himanshu1662.be22@gmail.com>
2024-02-19 12:43:07 +0530

    reducer updated

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,13 +2,14 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../Slices/authSlice"
 import profileReducer from "../Slices/profileSlice"
 import cartReducer from "../Slices/cartSlice"
-
+import viewCourseReducer from "../Slices/viewCourseSlice"
 import courseReducer from "../Slices/courseSlice"
 const rootReducer = combineReducers({
     auth : authReducer,
     profile:profileReducer,
     cart:cartReducer,
     course:courseReducer,
+    viewCourse:viewCourseReducer,
 })
 
 export default rootReducer;
